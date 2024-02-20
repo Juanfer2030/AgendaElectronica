bfe6777200cd1245e6a89a6497017b4988be76af
Juanfer20230 <jnfrbotero@gmail.com>
2024-02-19 21:43:40 -0500

    Se agrega el proyecto

---
+++ src/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,18 @@
+
+package agendaelectronica;
+
+import agendaelectronica.igu.Principal;
+
+
+public class AgendaElectronica {
+
+    
+    public static void main(String[] args) {
+        
+        Principal pantallaPrincipal = new Principal();
+        pantallaPrincipal.setVisible(true);
+        pantallaPrincipal.setLocationRelativeTo(null);
+        
+    }
+    
+}
