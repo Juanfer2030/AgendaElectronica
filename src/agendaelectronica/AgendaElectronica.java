45c4d14b4ae6d441f98fc8a991167c3448c66cd2
Juanfer20230 <jnfrbotero@gmail.com>
2024-02-27 20:45:06 -0500

    Se termina la agenda electronica sencilla

--- src/agendaelectronica/AgendaElectronica.java
+++ src/agendaelectronica/AgendaElectronica.java
@@ -8,7 +8,6 @@ public class AgendaElectronica {
 
     
     public static void main(String[] args) {
-        
         Principal pantallaPrincipal = new Principal();
         pantallaPrincipal.setVisible(true);
         pantallaPrincipal.setLocationRelativeTo(null);
